12878343931acb891e20769c8483c0fee5c12a60
tchi-sama <tchisama41@gmail.com>
2023-02-23 15:08:48 -0500

    done for redux

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import { remove, update  } from "./redux/userSlice";
 function App() {
   const user = useSelector(state=>state.user)
   const dispatch = useDispatch()
+
   const SwitchUser = ()=>{
+
     dispatch(update({email:"lora@gmail.com",name:"lora"}))
+
   }
   const ClearUser = ()=>{
     dispatch(remove())
